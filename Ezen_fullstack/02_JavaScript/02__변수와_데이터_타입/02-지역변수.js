7dbe505308215e5112062eb221b3446afbd0cc77
dodoisfree <yocasd2@gmail.com>
2022-01-24 21:43:53 +0900

    변수와 데이터 타입

---
+++ Ezen_fullstack/02_JavaScript/02__변수와_데이터_타입/02-지역변수.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// 1) 선언, 할당
+let myNumber1; // 선언
+
+myNumber1 = 100; // 할당
+console.log(myNumber1);
+
+// 2) 선언, 할당 통합
+let myNumber2 = 200;
+console.log(myNumber2);
+
+// 3) 중복서언 금지
+let myNumber3 = 100;
---
+++ Ezen_fullstack/02_JavaScript/02__변수와_데이터_타입/03-상수.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// 상수
+const MY_VALUE = 100;
