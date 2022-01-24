f88c15f470cd6b976b999cb628c9663de1b68247
dodoisfree <yocasd2@gmail.com>
2022-01-24 19:27:02 +0900

    readme

--- Ezen_fullstack/02_JavaScript/01_Hello_JavaScript/HelloWolrd.js
+++ Ezen_fullstack/02_JavaScript/01_Hello_JavaScript/HelloWolrd.js
@@ -6,10 +6,13 @@ console.group("MyMessage1");
     console.log("안녕하세요. Javascript2");
     console.log("안녕하세요. Javascript3");
 console.groupEnd();
-    console.group("MyMessage2");
-        console.log("안녕하세요. Javascript4");
-        console.log("안녕하세요. Javascript5");
+console.group("MyMessage2");
+    console.log("안녕하세요. Javascript4");
+    console.log("안녕하세요. Javascript5");
+    console.group("MyMessage2-1");
+        console.log("안녕하세요. Javascript6");
     console.groupEnd();
-        console.group("MyMessage2-1");
-            console.log("안녕하세요. Javascript6");
-        console.groupEnd();
+console.groupEnd();
+
+
+console.log("hello world");
