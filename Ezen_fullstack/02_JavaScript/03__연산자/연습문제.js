8d86a57cf5ec429ca5817df2a964f15bc7286390
dodoisfree <yocasd2@gmail.com>
2022-01-25 21:56:38 +0900

    연습문제 3,4

--- Ezen_fullstack/02_JavaScript/03__연산자/연습문제.js
+++ Ezen_fullstack/02_JavaScript/03__연산자/연습문제.js
@@ -3,10 +3,9 @@
 // age라는 이름의 지역변수에 할당하고 "나는 OO세 입니다." 라는 형식의 문장으로 이스케이프 문자를 활용하여 출력하시오. 
 
 const year1 = 2022;
-let age1 = 1994;
-let MyAge1 = year1 - age1;
+let age1 = year1 - 1994 + 1;
 
-console.log("나는 %d세 입니다.", MyAge1);
+console.log("나는 %d세 입니다.", age1);
 
 
   
@@ -14,10 +13,10 @@ console.log("나는 %d세 입니다.", MyAge1);
 // // // 자신의 나이를 의미하는 상수 age를 정의하고 이 값을 활용하여 자신이 태어난 년도 year를 전역 변수 형식으로 산출하여 
 // // // "나는 OOOO년도에 태어났습니다."라는 형식의 문장으로 이스케이프 문자를 활용하여 출력하시오.
 
-const Age2 = 28;
-var year2 = 2022 - Age2;
+const age = 29;
+var year = 2022 - (29-1);
 
-console.log("나는 %d년도에 태어났습니다.", year2);
+console.log("나는 %d년도에 태어났습니다.", year);
 
 
 
@@ -29,18 +28,25 @@ console.log("나는 %d년도에 태어났습니다.", year2);
 // // 필요한 바구니의 수를 구하는 프로그램을 구현하시오.
 
 const numOfApples = 123;
-const Basket = numOfApples >= 130 ? "14개" : "13개";
+const extra = (numOfApples % 10) / 10; // 123의 나머지 3을 나누기 10 하면 0.3
+console.log(extra);
 
-console.log("필요한 바구니는 %s 입니다.", Basket);
+let basketCount = (numOfApples / 10) - extra; // 123 나누기 10 하면 12
+basketCount += extra > 0 ? 1 : 0 ; // 0보다 extra(0.3)이 크면 basketCount 1 증가
+console.log(basketCount);
+// let basketCount = extra > 0 ? (numOfApples / 10) - extra + 1 : (numOfApples / 10) - extra;
+// console.log(basketCount);
 
 
 
 // // 문제 4)
 // // 어떤 계산기는 입력된 값에서 백의자리 이하를 버리고 결과를 도출한다고 한다. 
-// // 예를 들어 입력된 값이 456이라면 400이 결과로 출력된다. 이러한 출력결과를 만들 수 있는 코드를 작성하시오.
+// // 예를 들어 입력된 값이 457이라면 400이 결과로 출력된다. 이러한 출력결과를 만들 수 있는 코드를 작성하시오.
 
-const Result = 456;
-const Print = Result >= 400 ? "400" : "300";
+const number = 457;
+const extra = (number % 100) / 100; // number(457) % 100 은 57이다, 57에 / 100 하면 0.57
+console.log(extra);
 
-console.log("입력 값 456의 계산기 출력 값은 %s 입니다.", Print);
+const result = ((number / 100) - extra) * 100; // 457 / 100 = 4.57이다, -0.57 하고 * 100 해서 400이 된다. 
+console.log(result);
 
