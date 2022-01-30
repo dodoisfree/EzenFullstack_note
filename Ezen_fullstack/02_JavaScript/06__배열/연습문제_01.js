ad4ea7193506cd367d02a6768b24dfbcde088c05
dodoisfree <yocasd2@gmail.com>
2022-01-30 23:51:01 +0900

    연습문제1

--- Ezen_fullstack/02_JavaScript/06__배열/연습문제_01.js
+++ Ezen_fullstack/02_JavaScript/06__배열/연습문제_01.js
@@ -1,5 +1,5 @@
 var check_list = [true, false, false, true, false];
-let j = 0;
+
 console.log("before --> " + check_list);
 for (let i = 0; i < check_list.length; i++) {
     check_list[i] = !check_list[i];
