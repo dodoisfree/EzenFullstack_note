36ed373efacd8ecfd74e382272f74efd8dc2fc9a
gyeongjae cheon <yocasd2@gmail.com>
2022-05-07 18:59:14 +0900

    .

--- Ezen_fullstack/05_ReactJS/필기/src/App.js
+++ Ezen_fullstack/05_ReactJS/필기/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
+import Say from "Say";
 
 function App() {
-  const name = "리액트";
 
-  return ( 
-    <div>
-      {name == "리액트" && <h1>and 연산자 리액트입니다</h1>}
-    </div>
+  return (
+    <Say />
   );
 }
 
